104925929441dccd28bfd700dd10d6a89b236e63
thomas <=>
2018-10-08 16:49:18 -0300

    async await

--- async-demo/index.js
+++ async-demo/index.js
@@ -7,11 +7,28 @@ console.log('Before');
   })
 });
 */
+// Promise based approach
+/*
 getUser(1)
     .then(user => getRepositories(user.gitHubUsername))
     .then(repos => getCommits(repos[0]))
     .then(commits => console.log('Commits', commits))
     .catch(err => console.log('Error', err.message));
+*/
+// async await approach
+async function displayCommits() {
+    try {
+        const user = await getUser(1);
+        const repos = await getRepositories(user.gitHubUsername);
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    } catch(err) {
+        console.log('Error', err.message);
+    }
+    
+}
+
+displayCommits();
 
 console.log('After');
 
@@ -28,7 +45,8 @@ function getRepositories(username) {
     return new Promise((resolve,reject) => {
         setTimeout(() => {
             console.log('Calling GitHub API...');
-            resolve(['repo1', 'repo2', 'repo3']);
+            // resolve(['repo1', 'repo2', 'repo3']);
+            reject(new Error('Could not get repos'));
           }, 2000);
     });
   
