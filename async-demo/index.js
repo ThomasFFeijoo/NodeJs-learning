8d3224b56b9065e5077df0f79b3a806eed932cb5
thomas <=>
2018-10-08 13:28:49 -0300

    callback hell

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,31 @@
 console.log('Before');
-setTimeout(() => {
-    console.log("reading an user from a database");
-},2000);
+getUser(1, (user) => {
+    //console.log('User', user);
+    getRepositories(user.gitHubUsername, (repositories) => {
+        getCommits(repositories, (commits) => {
+            // CALLBACK HELL
+        })
+    })
+});
+
+
 console.log('After');
+
+// ways of dealing with async
+// Callbacks
+// Promises
+// async/await
+
+//callback will be called when the result is ready
+function getUser(id, callback) {
+    setTimeout(() => { //simulating an db connection
+        console.log("reading an user from a database");
+        callback({id: id, gitHubUsername: 'thomas'});
+    },2000);
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        callback(['repo1', 'repo2', 'repo3'])
+    },2000);
+}
