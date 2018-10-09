f0ab2fea1906f6fe390f9a2e969b5b6568bd511f
thomas <thomwe8@hotmail.com>
2018-10-09 12:07:58 -0300

    setting environment for Authentication and Autorization

--- vidly/models/customer.js
+++ vidly/models/customer.js
@@ -1,37 +1,34 @@
-const mongoose = require('mongoose');
 const Joi = require('joi');
+const mongoose = require('mongoose');
 
-const customerSchema = ;
-  
-  const Customer = mongoose.model('Customer', new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50
-    },
-    isGold: {
-        type: Boolean,
-        default: false
-    },
-    phone: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50
-    }
-  }));
-  
-
-  function validateCustomer(customer) {
-    const schema = {
-      name: Joi.string().min(5).max(50).required(),
-      phone: Joi.string().min(5).max(50).required(),
-      isGold: Joi.boolean()
-    };
-  
-    return Joi.validate(customer, schema);
+const Customer = mongoose.model('Customer', new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  isGold: {
+    type: Boolean,
+    default: false
+  },
+  phone: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
   }
+}));
 
-exports.Customer = Customer;
-exports.validate = validateCustomer; 
+function validateCustomer(customer) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    phone: Joi.string().min(5).max(50).required(),
+    isGold: Joi.boolean()
+  };
+
+  return Joi.validate(customer, schema);
+}
+
+exports.Customer = Customer; 
+exports.validate = validateCustomer;
