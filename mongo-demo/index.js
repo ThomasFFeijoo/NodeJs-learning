80b31a9b7564f464f06f832c059702aca5f822c8
thomas <=>
2018-10-09 08:37:38 -0300

    mongo model

---
+++ mongo-demo/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('connected to MongoDB'))
+    .catch(err => console.error('could not connect', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+const course = new Course({
+    name: 'Nodejs Course',
+    author: 'Thomas',
+    tags: [ 'node', 'backend'],
+    isPublished: true
+});
