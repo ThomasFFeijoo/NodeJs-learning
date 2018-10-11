a57310dfec40de1a35071e9ae69dab187cd6b7dd
thomas <thomwe8@hotmail.com>
2018-10-11 09:58:56 -0300

    testing strings

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -15,5 +15,13 @@ describe('absolute', () => {
         const result = lib.absolute(0);
         expect(result).toBe(0);
     });
-})
+});
+
+describe('greet', () => {
+    it('should return the greeting message', () => {
+        const result = lib.greet('Thomas');
+        expect(result).toMatch(/Thomas/);
+        expect(result).toContain('Thomas');
+    })
+});
 
